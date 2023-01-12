9b162cdaff134dc5355c6b74243bf5508fc9bcdc
Clóvis Magno <38765104+magnoClovis@users.noreply.github.com>
2023-01-12 11:09:18 +0100

    Primer Semestre
    Sincronización de los proyectos hechos en el primer semestre

---
+++ 5 - Adivinar Número/1 - Adivinar numero - Persona.py
@@ -0,0 +1,9 @@
+'''
+En este proyecto vamos a desarrollar un juego donde el ordenador genera
+un número aleatorio y nosotros necesitamos adivinarlo. Para ello, en cada
+intento que hagamos, el programa nos avisará si el número correcto es mayor
+o menor que el número que hemos introducido por teclado.
+
+A priori, el ordenador debe sortear números entre 0 y 10 para que sea más fácil
+el testeo y corrección de eventuales errores.
+'''
