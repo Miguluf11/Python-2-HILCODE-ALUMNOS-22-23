b1ffe0cffbe449ca871fbed296ed9c58767a4516
Miguluf11 <miguel.ufanomendoza@gmail.com>
2023-01-19 20:26:26 +0100

    Clase 19/01/2023
    Co-Authored-By: Clóvis Magno <38765104+magnoClovis@users.noreply.github.com>

--- 5 - Adivinar Número/2 - Adivinar numero - Ordenador.py
+++ 5 - Adivinar Número/2 - Adivinar numero - Ordenador.py
@@ -1,6 +1,6 @@
 '''
 Semejante al proyecto anterior, vamos a hacer otro proyecto
-para adivinar números, sin embargo, nosotros somos los que 
+para adivinar números, sin embargo, nosotros somos los que
 decimos que número debe ser adivinado y el ordenador es el
 responsáble por adivinarlo en el menor número de turnos posible.
 
@@ -10,3 +10,31 @@ Hay várias maneras de hacerlo, cabe a vosotros pensar y desarrollar
 una manera que el ordenador pueda encontrar el número secreto en el
 menor número de turnos posible.
 '''
+import random
+
+numero = int(input("Elige un número"))
+máximo = 10
+ordenador = random.randint(0,máximo)
+intentos = 0
+def juego(numero,ordenador,intentos):
+    print (ordenador)
+    while numero != ordenador:
+        if numero > ordenador:
+            intentos += 1
+            print ("El número es mayor")
+            ordenador = random.randint(0,máximo)
+            print(ordenador)
+        if numero < ordenador:
+            intentos += 1
+            print ("El número es menor")
+            ordenador = random.randint(0,máximo)
+            print (ordenador)
+        if numero == ordenador:
+            intentos  += 1
+            print (ordenador)
+            print("Es correcto")
+            print ("Lo has hecho en", intentos, "intentos")
+
+
+juego(numero,ordenador,intentos)
+
