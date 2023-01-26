21f5535bc01914f5063d405495d7a44014cbbbaf
clovisHilcode <38765104+magnoClovis@users.noreply.github.com>
2023-01-26 22:01:27 +0100

    Update 3 - Adivinar numero - Ordenador (optimizado).py

--- 5 - Adivinar Número/3 - Adivinar numero - Ordenador (optimizado).py
+++ 5 - Adivinar Número/3 - Adivinar numero - Ordenador (optimizado).py
@@ -1,33 +1,33 @@
 import random
 
-def input_numero(lim):
-    numero = int(input(f"Escriba un numero entre 0 y {lim} "))
-    while numero > lim or numero < 0:
+def input_numero(MAX):
+    numero = int(input(f"Escriba un numero entre 0 y {MAX} "))
+    while numero > MAX or numero < 0:
         print("Este número no es válido")
-        numero = int(input(f"Escriba un numero entre 0 y {lim} "))
+        numero = int(input(f"Escriba un numero entre 0 y {MAX} "))
     return numero
 
-def adivinar(lim, numero):
-    lim_inferior = 0
-    lim_superior = lim
-    ordenador = lim//2
-    repeticiones = 0
-    while ordenador != numero:
-        print("El numero ", ordenador," no es correcto.")
-        if ordenador > numero:
-            lim_superior = ordenador
-            ordenador = (lim_superior+lim_inferior)//2
+def adivinar(MAX, numero, intentos):
+    minimo = 0
+    maximo = MAX
+    bot = MAX//2
+    while bot != numero:
+        print("El numero ", bot," no es correcto.")
+        if bot > numero:
+            maximo = bot
+            bot = (maximo+minimo)//2
 
-        elif ordenador < numero:
-            lim_inferior = ordenador
-            ordenador = (lim_superior+lim_inferior)//2
+        elif bot < numero:
+            minimo = bot
+            bot = (maximo+minimo)//2
 
-        repeticiones = repeticiones + 1
-    return ordenador, repeticiones
+        intentos = intentos + 1
+    return bot, intentos
 
 
-lim = 1000
-numero = input_numero(lim)
-ordenador, repeticiones = adivinar(lim, numero)
-print("El numero ", ordenador," es correcto.")
-print("Repeticiones: ", repeticiones)
+MAX = 10000
+numero = input_numero(MAX)
+intentos = 0
+bot, intentos = adivinar(MAX, numero,intentos)
+print("El numero ", bot," es correcto.")
+print("Repeticiones: ", intentos)
