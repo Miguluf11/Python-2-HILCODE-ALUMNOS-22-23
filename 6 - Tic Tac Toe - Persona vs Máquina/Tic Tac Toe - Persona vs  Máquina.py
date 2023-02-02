bd9d679aee5ff73936844eae9573c990d8e46668
clovisHilcode <38765104+magnoClovis@users.noreply.github.com>
2023-02-02 18:24:40 +0100

    Create Tic Tac Toe - Persona vs  Máquina.py
    Instrucciones Tic Tac Toe - Persona vs Máquina

---
+++ 6 - Tic Tac Toe - Persona vs Máquina/Tic Tac Toe - Persona vs  Máquina.py
@@ -0,0 +1,14 @@
+'''
+Ahora que ya hemos treabajado en crear funciones y algoritmos algo más inteligentes,
+vamos a usar los mismos conceptos para crear un bot para que sea el adversario en el
+Tic Tac Toe y así el jugador pueda jugar contra la máquina.
+
+Para ello, vamos a tomar como base el juego que ya ha sido hecho en las clases anteriores.
+Podéis copiar todo el código del juego y pegarlo abajo para que empecemos a trabajar a partir de
+este código que ya está hecho. Una vez hecho esto, haremos una función que será la responsable por
+hacer con que la máquina elija una posición en la tabla y sea capaz de jugar contra el jugador.
+
+El resto del programa debe funcionar tal cual como antes: posiciones ya ocupadas no pueden ser elegidas
+otra vez por ningún de los jugadores, al final de cada turno debe aparecer quien gana, la puntuación total
+de cada jugador, preguntar si el jugador desea jugar una vez más, etc.
+'''
